11985eb027509a94ee7414ba1de1aebf7a60da10
= <=>
2025-10-08 08:55:12 +0200

    Első óra

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halmazállapot és név</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="form-container">
+            <h1>My First Excercise</h1>
+            <p>The first excersie's description</p>
+            <form action="">
+                <label for="username">Username:</label>
+                <input type="text" name="username" id="username">
+                <div class="form-group">
+                    <label for="water_temp">Water Temperature</label>
+                    <input type="number" name="water_temp" id="water_temp">
+                </div>
+                <div class="button-container">
+                    <button>Calculate the form of the water</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
