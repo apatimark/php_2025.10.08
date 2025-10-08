3c6b80c188cb641533312a0b4645ae0b61413ecd
= <=>
2025-10-08 09:34:26 +0200

    Első óra vége

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+    var_dump(value: $_GET);
+    $username = $_GET["username"];
+    $water_temp = $_GET["water_temp"];
+    $result;
+
+
+    switch ($water_temp) {
+        case $water_temp <= 0:
+            $result = "Ice";
+            break;
+        case $water_temp > 0 && $water_temp < 100:
+            $result = "Liquid";
+            break;
+        case $water_temp >= 100:
+            $result = "Gas";
+            break;
+        
+        default:
+            $result = "Error, check your data";
+            break;
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +35,7 @@
         <div class="form-container">
             <h1>My First Excercise</h1>
             <p>The first excersie's description</p>
-            <form action="">
+            <form action="#" method="GET">
                 <label for="username">Username:</label>
                 <input type="text" name="username" id="username">
                 <div class="form-group">
@@ -22,6 +46,8 @@
                     <button>Calculate the form of the water</button>
                 </div>
             </form>
+            <p><strong>Feladat megoldása: </strong></p>
+            <p><?php echo "The water's state is {$result}."?></p>
         </div>
     </div>
 </body>
